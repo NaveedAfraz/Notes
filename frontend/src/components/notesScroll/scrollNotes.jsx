2372f552657dfa41552accec3fbbc96aee9604ae
NaveedAfraz <naveedafraz123@gmail.com>
2025-01-31 11:02:21 +0530

    added

--- frontend/src/components/notesScroll/scrollNotes.jsx
+++ frontend/src/components/notesScroll/scrollNotes.jsx
@@ -3,23 +3,24 @@ import { useState } from "react";
 
 function NotesCarousel({ latestNotes }) {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const notesPerPage = 3; // Show 3 notes by default
 
+  const [notesPerPAge, setNotesPerPage] = useState(3);
   const handleNext = () => {
-    if (currentIndex + notesPerPage < latestNotes.length) {
-      setCurrentIndex(currentIndex + notesPerPage);
+    if (currentIndex + notesPerPAge < latestNotes.length) {
+      setCurrentIndex(currentIndex);
+      setNotesPerPage(notesPerPAge + 3);
     }
   };
 
   const handlePrev = () => {
-    if (currentIndex - notesPerPage >= 0) {
-      setCurrentIndex(currentIndex - notesPerPage);
+    if (currentIndex - notesPerPAge >= 0) {
+      setCurrentIndex(currentIndex);
+      setNotesPerPage(notesPerPAge - 3);
     }
   };
 
   return (
     <div className="relative flex w-full items-center bg-amber-300 p-4">
-      {/* Previous Button */}
       <button
         className="bg-gray-800 text-white p-2 rounded-full shadow-md mx-2 disabled:opacity-50 hidden sm:block"
         onClick={handlePrev}
@@ -28,22 +29,18 @@ function NotesCarousel({ latestNotes }) {
         ◀
       </button>
 
-      {/* Notes Display */}
       <div className="flex justify-center items-center gap-4 sm:gap-8 lg:gap-11 py-2 overflow-hidden w-full">
-        {latestNotes
-          .slice(currentIndex, currentIndex + notesPerPage)
-          .map((latestNote) => (
-            <div key={latestNote._id} className="w-64 sm:w-72">
-              <NoteCard note={latestNote} />
-            </div>
-          ))}
+        {latestNotes.slice(currentIndex, notesPerPAge).map((latestNote) => (
+          <div key={latestNote._id} className="w-64 sm:w-72">
+            <NoteCard note={latestNote} />
+          </div>
+        ))}
       </div>
 
-      {/* Next Button */}
       <button
         className="bg-gray-800 text-white p-2 rounded-full shadow-md mx-2 disabled:opacity-50 hidden sm:block"
         onClick={handleNext}
-        disabled={currentIndex + notesPerPage >= latestNotes.length}
+        disabled={currentIndex + notesPerPAge >= latestNotes.length}
       >
         ▶
       </button>
