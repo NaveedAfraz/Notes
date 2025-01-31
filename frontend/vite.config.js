a7a9e6399053d4113f3427b1f7e0afbb257ef228
NaveedAfraz <naveedafraz123@gmail.com>
2025-01-31 15:39:05 +0530

    added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },   
+  },
 });
