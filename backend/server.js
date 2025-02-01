f3f38e129578fd7125a76a1ffa07f8922c0d11c5
NaveedAfraz <naveedafraz123@gmail.com>
2025-02-01 12:16:36 +0530

    Fix configuration

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,11 @@
 const express = require("express");
+
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectToDatabase = require("./mongoDB");
-const path = require("path");
-const fs = require("fs");
-
-require("dotenv").config({ path: "../.env" });
-
 const app = express();
-
-
+const path = require("path");
+require("dotenv").config({ path: "../.env" });
 app.use(
   cors({
     origin: "https://notes-dt72.onrender.com",
@@ -22,20 +18,20 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 
-
 connectToDatabase();
-
 
 const userRoutes = require("./routes/auth/auth");
 const userNotes = require("./routes/Notes/userNotes");
 const requireAuth = require("./middleware/authCheck");
 const searchnotesRoute = require("./routes/search/search");
 
-// ✅ Serve static assets
-app.use(express.static(path.join(__dirname, "frontend/dist")));
-app.use("/assets", express.static(path.join(__dirname, "frontend/dist/assets")));
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "frontend/dist/assets"))
+);
 
-// Debug Route: List files in dist (for debugging only)
+// Serve the index.html file for all routes
+const fs = require("fs");
 app.get("/list-files", (req, res) => {
   const distPath = path.join(__dirname, "frontend/dist");
   fs.readdir(distPath, (err, files) => {
@@ -44,11 +40,11 @@ app.get("/list-files", (req, res) => {
   });
 });
 
-
 app.use("/auth", userRoutes);
 
 app.get("/reAuth/verify", requireAuth, (req, res) => {
   console.log(req.user);
+
   res.status(200).json({
     message: "hlo",
     user: {
@@ -58,21 +54,24 @@ app.get("/reAuth/verify", requireAuth, (req, res) => {
     },
   });
 });
-
-
 app.use(/^(?!\/auth|\/assets|\/$).*/, requireAuth);
 
 
 app.use("/userNotes", userNotes);
 app.use("/notesearch", searchnotesRoute);
 
-
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+  const indexPath = path.resolve(__dirname, "frontend/dist", "index.html");
+  console.log("Sending file from:", indexPath);
+  res.sendFile(indexPath, (err) => {
+    if (err) {
+      console.error("Error sending index.html:", err);
+      res.status(err.status).end();
+    }
+  });
 });
-
-// Start Server
 const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+  console.log("Server is running on port 3006");
 });
+
